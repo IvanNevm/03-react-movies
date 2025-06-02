542bc85f9be485f3ebf653cbfc6f4e46c2b342db
Ivan Nevm <ivannevmytyi@gmail.com>
2025-06-02 18:05:39 +0300

    ddd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_TOKEN: string;
+    // Можна оголосити й інші змінні, наприклад:
+    // readonly VITE_API_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
